c2d748b64e99ed4942a735c2da166edadee28310
CHOsen One <37858778+leoncho918@users.noreply.github.com>
2019-05-11 01:24:17 +1000

    Added method to generate random sequence of buttons

--- pixelmix_code/pixelmix_code.ino
+++ pixelmix_code/pixelmix_code.ino
@@ -8,3 +8,11 @@ void setup() {
 void loop() {
   
 }
+
+int[] generateSequence(int difficulty) {
+  int sequence[difficulty];
+  for (int i=0; i<sequence.size(); i++) {
+    sequence[i] = random(0, 15);
+  }
+  return sequence;
+}
